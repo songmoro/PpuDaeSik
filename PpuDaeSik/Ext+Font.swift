5bf60759d670fe55b2013b74087bdd394f1dc25b
songmoro <wlqgkr@naver.com>
2024-01-02 17:56:17 +0900

    feat: 폰트 관련 리소스 추가

---
+++ PpuDaeSik/Ext+Font.swift
@@ -0,0 +1,51 @@
+//
+//  Ext+Font.swift
+//  PpuDaeSik
+//
+//  Created by 송재훈 on 1/2/24.
+//
+
+import SwiftUI
+
+extension Font {
+    private func setFontSize() -> Double {
+        return switch UIScreen.screenHeight {
+        case 480.0:
+            0.85
+        case 568.0, 667.0:
+            0.9
+        case 736.0:
+            0.95
+        case 812.0:
+            0.98
+        case 844.0, 852.0:
+            1
+        case 896.0, 926.0:
+            1.05
+        case 932.0:
+            1.08
+        default:
+            1
+        }
+    }
+    
+    static func largeTitle() -> Font {
+        Font.custom("Pretendard-Regular", size: 24)
+    }
+    
+    static func title() -> Font {
+        Font.custom("Pretendard-Regular", size: 20)
+    }
+    
+    static func headline() -> Font {
+        Font.custom("Pretendard-Regular", size: 18)
+    }
+    
+    static func subhead() -> Font {
+        Font.custom("Pretendard-Regular", size: 16)
+    }
+    
+    static func body() -> Font {
+        Font.custom("Pretendard-Regular", size: 12)
+    }
+}
