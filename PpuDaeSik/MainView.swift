db827fe29d21fce5d19dd7125736a14e2d8be361
songmoro <wlqgkr@naver.com>
2024-01-03 01:16:01 +0900

    feat: 타이틀, 캠퍼스 컴포넌트 추가

--- PpuDaeSik/MainView.swift
+++ PpuDaeSik/MainView.swift
@@ -7,9 +7,73 @@
 
 import SwiftUI
 
+enum Campus: String, CaseIterable {
+    case busan = "부산"
+    case milyang = "밀양"
+    case yangsan = "양산"
+}
+
 struct MainView: View {
+    @State var selectedCampus = "부산"
+    @State var isSheetShow = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray100.ignoresSafeArea()
+            
+            VStack {
+                title
+                campus
+                Spacer()
+            }
+        }
+    }
+    
+    var title: some View {
+        HStack {
+            Image(isSheetShow ? "LogoEye" : "Logo")
+                .resizable()
+                .frame(width: UIScreen.getWidth(50), height: UIScreen.getHeight(50))
+            
+            Text("뿌대식")
+                .font(.largeTitle())
+            
+            Spacer()
+            
+            Button {
+                isSheetShow = true
+            } label: {
+                Image(systemName: "gearshape.fill")
+                    .font(.largeTitle())
+                    .foregroundColor(.blue100)
+            }
+        }
+        .padding(.horizontal, UIScreen.getWidth(18))
+        .padding(.bottom, UIScreen.getHeight(20))
+    }
+    
+    var campus: some View {
+        HStack(spacing: 0) {
+            ForEach(Campus.allCases, id: \.self) { location in
+                Button {
+                    selectedCampus = location.rawValue
+                } label: {
+                    Text("\(location.rawValue)")
+                        .foregroundColor(location.rawValue == selectedCampus ? .black100 : .black40)
+                        .padding(.bottom, UIScreen.getHeight(6))
+                        .overlay(alignment: .bottom) {
+                            Rectangle()
+                                .foregroundColor(location.rawValue == selectedCampus ? .blue100 : .clear)
+                                .frame(height: UIScreen.getHeight(3))
+                        }
+                }
+                .padding(.trailing, UIScreen.getWidth(18))
+            }
+            .font(.title())
+            
+            Spacer()
+        }
+        .padding(.horizontal, UIScreen.getWidth(18))
     }
 }
 
