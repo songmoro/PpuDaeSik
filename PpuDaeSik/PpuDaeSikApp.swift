1cd463d7b9bc15bc1be5ce5ba220464520e92a35
songmoro <wlqgkr@naver.com>
2024-01-04 00:32:08 +0900

    feat: Mainview 시작으로 변경

--- PpuDaeSik/PpuDaeSikApp.swift
+++ PpuDaeSik/PpuDaeSikApp.swift
@@ -13,8 +13,8 @@ struct PpuDaeSikApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
