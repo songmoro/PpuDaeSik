647b99ebe776d87b71696ef73fabe0a5bc09a646
songmoro <wlqgkr@naver.com>
2024-01-10 01:37:30 +0900

    feat: darkGray 추가

--- PpuDaeSik/Ext/Ext+Color.swift
+++ PpuDaeSik/Ext/Ext+Color.swift
@@ -28,6 +28,7 @@ extension Color {
     static let white100 = Color(hex: "FFFFFF")
     static let white40 = Color(hex: "FFFFFF").opacity(0.4)
     static let gray100 = Color(hex: "F9F9F9")
+    static let darkGray100 = Color(hex: "B5B5B5")
     static let blue100 = Color(hex: "00AAFF")
     static let yellow100 = Color(hex: "FFCC00")
 }
