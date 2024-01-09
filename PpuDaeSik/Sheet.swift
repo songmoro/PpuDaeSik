099c42f4e3fbf2078d4ffa0148911f0627ba21a4
songmoro <wlqgkr@naver.com>
2024-01-10 01:37:49 +0900

    feat: 시트 추가

---
+++ PpuDaeSik/Sheet.swift
@@ -0,0 +1,46 @@
+//
+//  Sheet.swift
+//  PpuDaeSik
+//
+//  Created by 송재훈 on 1/10/24.
+//
+
+import SwiftUI
+
+struct Sheet: View {
+    @State var selectedCampus = Campus.부산
+    
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 2.5)
+                .foregroundColor(.darkGray100)
+                .frame(width: UIScreen.getWidth(36), height: UIScreen.getHeight(5))
+            
+            HStack {
+                Text("기본 캠퍼스")
+                    .foregroundColor(.black100)
+                
+                Spacer()
+                
+                Picker(selection: $selectedCampus) {
+                    ForEach(Campus.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                    .foregroundColor(.blue100)
+                } label: { }
+                    .pickerStyle(.menu)
+                
+            }
+            .font(.headline())
+            
+            Spacer()
+        }
+        .padding(.top)
+        .frame(width: UIScreen.getWidth(350))
+        .presentationDetents([.height(UIScreen.getHeight(238))])
+    }
+}
+
+#Preview {
+    Sheet()
+}
