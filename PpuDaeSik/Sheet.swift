621b6ca143da7d35326d0a1e6c15d73589b16ab9
songmoro <wlqgkr@naver.com>
2024-01-11 15:15:28 +0900

    feat: 시트 다크 모드 대응

--- PpuDaeSik/Sheet.swift
+++ PpuDaeSik/Sheet.swift
@@ -11,34 +11,38 @@ struct Sheet: View {
     @Binding var defaultCampus: String
     
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 2.5)
-                .foregroundColor(.darkGray100)
-                .frame(width: UIScreen.getWidth(36), height: UIScreen.getHeight(5))
+        ZStack {
+            Color.gray100.ignoresSafeArea()
             
-            HStack {
-                Text("기본 캠퍼스")
-                    .foregroundColor(.black100)
+            VStack {
+                RoundedRectangle(cornerRadius: 2.5)
+                    .foregroundColor(.darkGray100)
+                    .frame(width: UIScreen.getWidth(36), height: UIScreen.getHeight(5))
+                
+                HStack {
+                    Text("기본 캠퍼스")
+                        .foregroundColor(.black100)
+                    
+                    Spacer()
+                    
+                    Picker(selection: $defaultCampus) {
+                        ForEach(Campus.allCases, id: \.self) {
+                            Text($0.rawValue)
+                                .tag($0.rawValue)
+                        }
+                        .foregroundColor(.blue100)
+                    } label: { }
+                        .pickerStyle(.menu)
+                    
+                }
+                .font(.headline())
                 
                 Spacer()
-                
-                Picker(selection: $defaultCampus) {
-                    ForEach(Campus.allCases, id: \.self) {
-                        Text($0.rawValue)
-                            .tag($0.rawValue)
-                    }
-                    .foregroundColor(.blue100)
-                } label: { }
-                    .pickerStyle(.menu)
-                
             }
-            .font(.headline())
-            
-            Spacer()
+            .padding(.top)
+            .frame(width: UIScreen.getWidth(350))
+            .presentationDetents([.height(UIScreen.getHeight(238))])
         }
-        .padding(.top)
-        .frame(width: UIScreen.getWidth(350))
-        .presentationDetents([.height(UIScreen.getHeight(238))])
     }
 }
 
