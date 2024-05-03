20ff48220ff12e055fc2150b64712e68eaf429ef
songmoro <wlqgkr@naver.com>
2024-05-03 16:41:40 +0900

    feat: timeline 1시간 주기 수정

--- PpuDaeSikWidget/PpuDaeSikWidget.swift
+++ PpuDaeSikWidget/PpuDaeSikWidget.swift
@@ -19,17 +19,11 @@ struct Provider: TimelineProvider {
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
+        let nextRefreshDate = Calendar.current.date(byAdding: .hour, value: 1, to: currentDate)!
+        let entry = SimpleEntry(date: currentDate, emoji: "😀")
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let timeline = Timeline(entries: [entry], policy: .after(nextRefreshDate))
         completion(timeline)
     }
 }
