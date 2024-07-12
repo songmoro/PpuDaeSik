c8a30abe76f74ee6e07f3820db35a709f8dbbd09
songmoro <wlqgkr@naver.com>
2024-07-13 03:09:50 +0900

    feat: 이번 주 일 구하는 함수 extension화

--- PpuDaeSik/MainViewModel.swift
+++ PpuDaeSik/MainViewModel.swift
@@ -30,16 +30,7 @@ class MainViewModel: ObservableObject {
             return calendar
         }()
         
-        let interval: [Int] = switch calendar.component(.weekday, from: Date()) {
-        case 1: [ 0,  1,  2,  3,  4,  5,  6]
-        case 2: [-1,  0,  1,  2,  3,  4,  5]
-        case 3: [-2, -1,  0,  1,  2,  3,  4]
-        case 4: [-3, -2, -1,  0,  1,  2,  3]
-        case 5: [-4, -3, -2, -1,  0,  1,  2]
-        case 6: [-5, -4, -3, -2, -1,  0,  1]
-        case 7: [-6, -5, -4, -3, -2, -1,  0]
-        default: []
-        }
+        let interval = calendar.interval()
         
         let week = interval.map {
             let date = calendar.date(byAdding: .day, value: $0, to: Date())
