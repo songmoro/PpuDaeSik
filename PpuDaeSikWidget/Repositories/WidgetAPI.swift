52f4ff8dafcc91ba3820913fc4a03c0866e539a1
songmoro <wlqgkr@naver.com>
2024-11-25 20:03:03 +0900

    feat: 공유 모델 분리 및 재생성

--- PpuDaeSikWidget/Repositories/WidgetAPI.swift
+++ PpuDaeSikWidget/Repositories/WidgetAPI.swift
@@ -1,60 +1,60 @@
+////
+////  WidgetAPI.swift
+////  PpuDaeSikWidgetExtension
+////
+////  Created by 송재훈 on 5/3/24.
+////
 //
-//  WidgetAPI.swift
-//  PpuDaeSikWidgetExtension
+//import SwiftUI
+//import Moya
 //
-//  Created by 송재훈 on 5/3/24.
+//enum WidgetAPI {
+//    case checkStatus
+//    case query(queryType: QueryType, cafeteria: Cafeteria, _ categoty: String)
+//}
 //
-
-import SwiftUI
-import Moya
-
-enum WidgetAPI {
-    case checkStatus
-    case query(queryType: QueryType, cafeteria: Cafeteria, _ categoty: String)
-}
-
-extension WidgetAPI: TargetType {
-    var baseURL: URL {
-        let url = "https://api.notion.com/v1"
-        guard let baseURL = URL(string: url) else { fatalError() }
-        return baseURL
-    }
-    
-    var path: String {
-        switch self {
-        case .checkStatus:
-            return NotionDatabase.status.path()
-        case .query(let queryType, let cafeteria, _):
-            switch cafeteria {
-            case .금정회관교직원식당, .금정회관학생식당, .샛벌회관식당, .학생회관학생식당, .학생회관밀양학생식당, .학생회관밀양교직원식당, .편의동2층양산식당:
-                return NotionDatabase.restaurant(isUpdating: queryType.isUpdating()).path()
-            case .진리관, .웅비관, .자유관, .비마관, .행림관:
-                return NotionDatabase.domitory(isUpdating: queryType.isUpdating()).path()
-            }
-        }
-    }
-    
-    var method: Moya.Method {
-        .post
-    }
-    
-    var task: Moya.Task {
-        switch self {
-        case .checkStatus:
-            return .requestPlain
-        case .query(let queryType, let cafeteria, let category):
-            let data: FilterByCampusRequest = switch cafeteria {
-            case .금정회관교직원식당, .금정회관학생식당, .샛벌회관식당, .학생회관학생식당, .학생회관밀양학생식당, .학생회관밀양교직원식당, .편의동2층양산식당:
-                FilterByCampusRequest(queryType: queryType, name: cafeteria.code, category: category)
-            case .진리관, .웅비관, .자유관, .비마관, .행림관:
-                FilterByCampusRequest(queryType: queryType, name: cafeteria.code, category: category)
-            }
-            
-            return .requestJSONEncodable(data)
-        }
-    }
-    
-    var headers: [String: String]? {
-        MoyaHeader.Notion.header()
-    }
-}
+//extension WidgetAPI: TargetType {
+//    var baseURL: URL {
+//        let url = "https://api.notion.com/v1"
+//        guard let baseURL = URL(string: url) else { fatalError() }
+//        return baseURL
+//    }
+//    
+//    var path: String {
+//        switch self {
+//        case .checkStatus:
+//            return NotionDatabase.status.path()
+//        case .query(let queryType, let cafeteria, _):
+//            switch cafeteria {
+//            case .금정회관교직원식당, .금정회관학생식당, .샛벌회관식당, .학생회관학생식당, .학생회관밀양학생식당, .학생회관밀양교직원식당, .편의동2층양산식당:
+//                return NotionDatabase.restaurant(isUpdating: queryType.isUpdating()).path()
+//            case .진리관, .웅비관, .자유관, .비마관, .행림관:
+//                return NotionDatabase.domitory(isUpdating: queryType.isUpdating()).path()
+//            }
+//        }
+//    }
+//    
+//    var method: Moya.Method {
+//        .post
+//    }
+//    
+//    var task: Moya.Task {
+//        switch self {
+//        case .checkStatus:
+//            return .requestPlain
+//        case .query(let queryType, let cafeteria, let category):
+//            let data: FilterByCampusRequest = switch cafeteria {
+//            case .금정회관교직원식당, .금정회관학생식당, .샛벌회관식당, .학생회관학생식당, .학생회관밀양학생식당, .학생회관밀양교직원식당, .편의동2층양산식당:
+//                FilterByCampusRequest(queryType: queryType, name: cafeteria.code, category: category)
+//            case .진리관, .웅비관, .자유관, .비마관, .행림관:
+//                FilterByCampusRequest(queryType: queryType, name: cafeteria.code, category: category)
+//            }
+//            
+//            return .requestJSONEncodable(data)
+//        }
+//    }
+//    
+//    var headers: [String: String]? {
+//        MoyaHeader.Notion.header()
+//    }
+//}
