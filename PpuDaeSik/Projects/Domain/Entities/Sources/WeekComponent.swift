cd88f8ed8eb165f8383e72cbb0f936980228d5b1
songmoro <wlqgkr@naver.com>
2025-06-20 16:51:24 +0900

    init/내부 메소드 접근 제어자 수정

--- PpuDaeSik/Projects/Domain/Entities/Sources/WeekComponent.swift
+++ PpuDaeSik/Projects/Domain/Entities/Sources/WeekComponent.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 public struct WeekComponent {
     public let dayComponent: DayComponent
-    let date: Date
-
+    private let date: Date
+    
     public var dayValue: Int {
         Calendar.current.component(.day, from: date)
     }
@@ -41,10 +41,10 @@ public extension WeekComponent {
 
 extension WeekComponent: Equatable, Comparable {
     public static func == (lhs: WeekComponent, rhs: WeekComponent) -> Bool {
-        lhs.dayComponent.calendarWeekdayIndex == rhs.dayComponent.calendarWeekdayIndex
+        lhs.dayComponent == rhs.dayComponent
     }
     
     public static func < (lhs: WeekComponent, rhs: WeekComponent) -> Bool {
-        lhs.dayComponent.calendarWeekdayIndex < rhs.dayComponent.calendarWeekdayIndex
+        lhs.dayComponent < rhs.dayComponent
     }
 }
