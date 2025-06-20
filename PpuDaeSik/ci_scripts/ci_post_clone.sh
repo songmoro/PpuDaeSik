53f4ad3f534365bf45b250dc07cc35626e173658
songmoro <wlqgkr@naver.com>
2025-06-21 03:09:41 +0900

    xcode cloud secrets key append

--- PpuDaeSik/ci_scripts/ci_post_clone.sh
+++ PpuDaeSik/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+mkdir -p "$SECRET_XCCONFIG_PATH"
+
+cat > "$SECRET_XCCONFIG_FILE" <<EOF
+LOGGER_API_KEY=${SECRETS_XCCONFIG}
+EOF
+
 curl https://mise.jdx.dev/install.sh | sh
 eval "$(~/.local/bin/mise activate zsh)"
 
